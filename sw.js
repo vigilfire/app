3161b183bcc04ec57555ec8c8aba87122b3daa11
Sapphorion <sapphorion@gmail.com>
2026-09-12 17:21:30 +0200

    sign-off verification moved

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
    Bump CACHE (e.g. -v2) whenever index.html or sw.js changes so clients pick
    up the new version. */
 
-const CACHE = 'vigil-fire-v14';
+const CACHE = 'vigil-fire-v15';
 
 const LOCAL_SHELL = [
   './index.html',
