d01ea8da9cf4e1d74161e19e92033b2e8a0cd0cc
Sapphorion <sapphorion@gmail.com>
2026-09-15 21:32:05 +0200

    tools check assign

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
    Bump CACHE (e.g. -v2) whenever index.html or sw.js changes so clients pick
    up the new version. */
 
-const CACHE = 'vigil-fire-v20';
+const CACHE = 'vigil-fire-v21';
 
 const LOCAL_SHELL = [
   './index.html',
